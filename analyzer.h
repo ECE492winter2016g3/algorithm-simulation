9b0a9974e2d5cc5a6597602a88c8c96b134446a7
stravant <stravant@gmail.com>
2016-04-08 14:20:28 -0600

    Removed old stuff

--- analyzer.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "util.h"
-
-namespace Analyzer {
-	void scanSweep(coord<double> source, double angle, vector<line<double>> shape, vector<float> &angles, vector<float> &distances);
-	coord<double> scan(coord<double> source, double angle, vector<line<double>> shape);
-	vector<line<double>> getMap(vector<distance_measurement> measurements);
-};
